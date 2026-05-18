e8deeb96939161388e84bc40e6b9415a26f8a877
Secret-Doughnut <dustinorangecook@gmail.com>
2026-05-18 15:14:07 -0600

    Hopefully fixed an issue.

--- week3/scripts/modules.mjs
+++ week3/scripts/modules.mjs
@@ -2,6 +2,7 @@ import byuiCourse from "./course.mjs";
 import { setSectionSelection } from "./sections.mjs";
 import { setTitle, renderSections } from "./output.mjs";
 
+
 document.querySelector("#enrollStudent").addEventListener("click", function () {
   const sectionNum = Number(document.querySelector("#sectionNumber").value);
     byuiCourse.changeEnrollment(sectionNum);

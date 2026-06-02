a82da146eadda0f446b9aa2121afabdd337f0cc8
Secret-Doughnut <dustinorangecook@gmail.com>
2026-06-01 18:01:08 -0600

    Testing.

--- Practice/scripts/navigation.js
+++
@@ -1,8 +0,0 @@
-const navbutton = document.querySelector('#ham-btn');
-const navbar = document.querySelector('#nav-bar');
-
-navbutton.addEventListener('click', () => {
-    navbutton.classList.toggle('show');
-    navbar.classList.toggle('show');
-})
-

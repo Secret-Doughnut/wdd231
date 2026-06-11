78a8138f54d8fc0969da20edb295f1ff55760749
Secret-Doughnut <dustinorangecook@gmail.com>
2026-06-11 14:11:29 -0600

    Fixed some small issues the audit tool found.

--- finalproject/scripts/a.mjs
+++ finalproject/scripts/a.mjs
@@ -46,3 +46,86 @@ async function snakeDataFetch() {
         console.log(error);
     }
 }
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('show');
+    navigation.classList.toggle('show');
+})
+
+currentyear.innerHTML = `<span> ©${today.getFullYear()}<span>`
+
+modified.innerHTML = document.lastModified
+
+close.addEventListener('click', () => {
+    modal.close();
+})
+
+function createAnimalFactCard(animals) {
+
+    animals.facts.forEach(animal => {
+        let cardContainer = document.createElement("div");
+        let redBox = document.createElement("div");
+        let imageContainer = document.createElement("img");
+        let textContainer = document.createElement("p");
+        let learnMoreButton = document.createElement("button");
+
+        cardContainer.classList.add("card");
+        redBox.classList.add("fact");
+        learnMoreButton.classList.add(animal.extraClass);
+        learnMoreButton.classList.add("info-button");
+
+
+        learnMoreButton.textContent = `Learn More`
+        textContainer.textContent = `${animal.text}`
+        imageContainer.setAttribute("src", animal.image);
+        imageContainer.setAttribute("alt", animal.alt);
+        imageContainer.setAttribute("loading", "lazy");
+
+        redBox.appendChild(textContainer);
+        redBox.appendChild(learnMoreButton);
+
+        cardContainer.appendChild(imageContainer);
+        cardContainer.appendChild(redBox);
+
+        cardHolder.appendChild(cardContainer);
+    });
+}
+
+let answer = await bunnyDataFetch();
+createAnimalFactCard(answer);
+const infoButton = document.querySelectorAll('.info-button');
+
+
+infoButton.forEach((button) => {
+    button.addEventListener('click', async () => {
+
+        
+        let animal = "";
+        // let bunnyCheck = bunnyfacts.find(item => item.extraClass === button.classList[0]);
+        // let snakeCheck = snakefacts.find(item => item.extraClass === button.classList[0]);
+
+        // if (bunnyCheck) {
+        //     grabAnimalCredit(bunnyCheck);
+        // }
+        // else if (snakeCheck) {
+        //     grabAnimalCredit(snakeCheck);
+        // }
+
+        if (bunnyCard) {
+            let bunny = await bunnyDataFetch();
+            console.log(bunny);
+            animal = bunny.find(item => item.extraClass === button.classList[0]);
+            grabAnimalCredit(animal);
+            
+        }
+        modal.showModal();
+    })
+})
+
+const grabAnimalCredit = (animal) => {
+    title.textContent = animal.title;
+    source.textContent = animal.nameOfSource;
+    source.href = animal.source;
+    credit.textContent = animal.author;
+    credit.href = animal.imageCredit;
+}
